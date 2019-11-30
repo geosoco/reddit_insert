4671766d2e19d7ca58e6842aaaefff2425ed61bf
John Robinson <soco@uw.edu>
2019-11-30 15:11:48 -0800

    add script for oob extract

---
+++ do_extract_oob_data.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+if [ $# -lt 2  ]
+then
+	echo "usage: $0 <input file> <output path>"
+	exit 1
+fi
+
+while read F; do
+    filename="$(basename -- "$F")"
+    ext="${filename##*.}"
+
+    echo
+    echo
+    echo "========================"
+    echo $filename
+    echo "========================"
+    echo
+
+    retval=0
+    if [ $ext = 'bz2' ]; then
+        python -u reddit_part_copy.py "$filename" "$2"
+        retval=${PIPESTATUS[0]}
+    elif [ $ext = 'xz' ]; then
+        python -u reddit_part_copy.py "$filename" "$2"
+        retval=${PIPESTATUS[0]}
+    elif [ $ext = 'zst' ]; then
+        python -u reddit_part_copy.py "$filename" "$2"
+        retval=${PIPESTATUS[0]}
+    else
+        echo "UNKNOWN EXTENSION: $ext"
+        exit 1
+    fi
+
+
+    if [ $retval -ne 0 ]; then
+        echo "ERROR"
+        exit $retval
+    fi
+    
+    #echo "$filename ... $ext}"
+
+done <$1
+
